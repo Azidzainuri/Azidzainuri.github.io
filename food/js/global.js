6a938a726f137514f1ad249f2dd6f2537b6a8ed8
Azidzainuri <zainuriazid@gmail.com>
2023-07-16 18:06:30 +0700

    Update global.js

--- food/js/global.js
+++ food/js/global.js
@@ -151,5 +151,19 @@ $.getJSON(linkdata, function(data) {
       html += '</article>';
     }
     $("#htmlproduk").html(html);
+    $("#htmlproduk").owlCarousel({
+        stagePadding: 0,
+        loop: false,
+        autoplayHoverPause: false,
+        autoplay: false,
+        margin: 0,
+        nav: true,
+        navText: ['<svg viewBox="0 0 24 24"><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"/></svg>', '<svg viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>'],
+        responsive: { 
+          0: { items: 1, },
+          787: { items: 2 },
+          901: { items: 3 }
+        },
+    });
   }
 });
